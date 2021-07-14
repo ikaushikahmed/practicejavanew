4ddbf2c2c4884a5af083e63a10d835b32d56a2e4
ikaushikahmed <60728919+ikaushikahmed@users.noreply.github.com>
2021-07-14 17:22:03 -0400

    loop

---
+++ src/com/kaushikCoreJava/BasicSyntax/ForLoop.java
@@ -0,0 +1,20 @@
+package com.kaushikCoreJava.BasicSyntax;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++) {
+
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("* ");
+
+            }
+
+                System.out.println();
+
+        }
+
+    }
+}
